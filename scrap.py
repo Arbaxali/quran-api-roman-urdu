bce4b633bf23991a7fd44a11a7f794fa3ede3920
Arbaz ali <30619186+Arbaxali@users.noreply.github.com>
2022-10-22 00:07:02 +0530

    update1 (adding random)

--- scrap.py
+++ scrap.py
@@ -2,24 +2,43 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import codecs
+import random
 
-URL = "https://quranromanurdu.com/chapter/1"
+
+y = random.randrange(114)
+urll = "https://quranromanurdu.com/chapter/{}".format(y)
+print(urll)
+
+URL = urll
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
 table = soup.findAll('div',attrs={"id":"contentpara"})
 
+
+
 values = list(filter(None, table[0].text.split('\n')))
+
 values = list(filter(None, [value.replace("\xa0", "") for value in values[1:]]))
 
 d = {}
 for item in values:
-    key, value = item.split('.', maxsplit=1)
+    key, value = item.split('.' ,maxsplit = 1)
     d[key] = value
 
-print(d)    
+print(d)
+k = list(d.keys())
+maxx = int(max(k))
+minn  =int(min(k))
+
+
+randayahno = random.randrange(minn,maxx)
+srandayahno = str(randayahno)
+ayah = d.get(srandayahno, "not found")
 
 
 
-
+print("chapter: {}".format(y))
+print("ayah no: {}".format(srandayahno))
+print("ayah : {}".format(ayah))
